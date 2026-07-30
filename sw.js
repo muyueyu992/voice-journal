049b42862a244f2eb6f43e33b6d3995bb11e6d1e
muyueyu992 <853743466@139.com>
2026-07-30 10:21:00 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'voice-journal-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/app.js',
+  './js/ai.js',
+  './js/storage.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+});
